416a466ce0905b9c275f2cb8c6239f11770cb7bb
JenniferGarrett <jennifer@techtonicgroup.com>
2017-01-27 14:16:29 -0700

    new date stuffs

--- js/apprentice-page.js
+++ js/apprentice-page.js
@@ -4,25 +4,7 @@ function addNewPost() {
     var blogTitle = $('#blog_title').val();
     var blogContent = $('#blog_content').val();
 
-    var today = new Date();
-    var day = today.getDate();
-    var month = today.getMonth();
-    var yyyy = today.getFullYear();
-    var months = new Array(12);
-    months[0] = "January";
-    months[1] = "February";
-    months[2] = "March";
-    months[3] = "April";
-    months[4] = "May";
-    months[5] = "June";
-    months[6] = "July";
-    months[7] = "August";
-    months[8] = "September";
-    months[9] = "October";
-    months[10] = "November";
-    months[11] = "December";
-
-    today = months[month] + ' ' + day + ', ' + yyyy;
+    var today = new Date().toLocaleString('en-us', {month: 'long', day: 'numeric', year: 'numeric'});
 
     var newPost = `
             <div class="blog-wrapper">
